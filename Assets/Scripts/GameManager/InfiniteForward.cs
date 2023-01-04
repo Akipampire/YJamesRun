f30642c41fef66fce4af4ba48cff6eab1bdb486d
Virgil <virgil@nosoft.io>
2023-01-04 09:33:32 +0100

    Change Wall Color

--- Assets/Scripts/GameManager/InfiniteForward.cs
+++ Assets/Scripts/GameManager/InfiniteForward.cs
@@ -70,7 +70,6 @@ public class InfiniteForward : MonoBehaviour
         //Walls
         Instantiate(WallsRight[Random.Range(0, WallsRight.Length)], new Vector3(15, 5, currentZAxis + 5), Quaternion.Euler(0, 0, 90),newChunck.transform);
         Instantiate(WallsLeft[Random.Range(0, WallsLeft.Length)], new Vector3(-15, 5, currentZAxis + 5), Quaternion.Euler(0, 0, -90), newChunck.transform);
-
         //Spawn obstacles on random lanes
         SpawnObstacle(newChunck);
         if (Random.Range(0, 100) <= coinSpawnChance) SpawnCoin(newChunck);
