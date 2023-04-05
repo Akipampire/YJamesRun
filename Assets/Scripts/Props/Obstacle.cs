10d13953277d37ffa2a106efeb35b365d8646e2d
Virgil <virgil@nosoft.io>
2023-04-05 16:08:56 +0200

    fix

--- Assets/Scripts/Props/Obstacle.cs
+++ Assets/Scripts/Props/Obstacle.cs
@@ -10,7 +10,9 @@ public enum ESQUIVE_TYPE {
 }
 public class Obstacle : MonoBehaviour {
     [SerializeField] public ESQUIVE_TYPE[] typeEsquive;
-    private List<Player> playersHit = new List<Player>();
+    [SerializeField] public BoxCollider toDesactivate;
+	private List<Player> playersHit = new List<Player>();
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer == Mathf.Log(GameManager.PlayerLayer.value, 2)) {
 			Player playerHit = other.GetComponent<Player>();
@@ -18,7 +20,11 @@ public class Obstacle : MonoBehaviour {
 				GameManager.Instance.PlaySFX(SFXPlayer.SFX_TYPE.Hit);
 				playersHit.Add(playerHit);
                 playersHit.Last().OnHit(typeEsquive);
+                if (toDesactivate) toDesactivate.enabled = false;
 			}
 		}
     }
+	private void OnTriggerExit(Collider other) {
+		if (toDesactivate) toDesactivate.enabled = true;
+	}
 }
