e45770851fe6d2332bcbdab86c9fb6d9a4f40913
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2024-02-14 16:03:28 +0100

    test

--- Assets/Scripts/PowerUp/Speed.cs
+++ Assets/Scripts/PowerUp/Speed.cs
@@ -1,20 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public class Speed : PowerUP
 {
-    [SerializeField] float speed_duration = 3f;
-    [SerializeField] float speed_boost = 2f;
-    public void Start() {
-        GameManager.SPEED_BOOST_DURATION = speed_duration;
-    }
-    public override void Activate(GameObject trigger = null) {
-        trigger.GetComponent<PlayerPowerUp>().AddPowerUpToInventory(this);
-        Destroy(gameObject);
+    [SerializeField] private float speed_duration = 3f; // La durée pendant laquelle la vitesse augmente
+    [SerializeField] private float speed_boost = 2f; // Le facteur de vitesse final à atteindre
+    [SerializeField] private float accelerationFactor = 3f; // Facteur d'accélération pour augmenter l'effet
+
+    public override void Activate(GameObject trigger = null)
+    {
+        if (trigger != null)
+        {
+            trigger.GetComponent<PlayerPowerUp>().AddPowerUpToInventory(this);
+            Destroy(gameObject); // Détruit le power-up après activation
+        }
     }
 
-    public override void OnUse(Player user) {
-        user.GetComponent<PlayerMovement>().forwardSpeed = GameManager.Instance.infiniteForward.maxPlayerSpeed * speed_boost;
-        GameManager.Instance.PlaySFX(SFXPlayer.SFX_TYPE.SpeedUp);
-        GameManager.Instance.ResetSpeed(user);
+    public override void OnUse(Player user)
+    {
+        StartCoroutine(ApplySpeedBoost(user));
+    }
+
+    private IEnumerator ApplySpeedBoost(Player user)
+    {
+        float elapsedTime = 1;
+        float startSpeed = user.GetComponent<PlayerMovement>().forwardSpeed;
+        float targetSpeed = startSpeed * speed_boost; // Calcul de la vitesse cible
+
+        while (elapsedTime < speed_duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float currentBoost = Mathf.Pow(elapsedTime / speed_duration, accelerationFactor) * (targetSpeed - startSpeed);
+            user.GetComponent<PlayerMovement>().forwardSpeed = startSpeed + currentBoost;
+            yield return null; // Attend jusqu'au prochain frame
+        }
+
+        // Ici, la vitesse est augmentée de manière plus significative vers la fin de la période
+        user.GetComponent<PlayerMovement>().forwardSpeed = targetSpeed; // S'assure que la vitesse cible est atteinte
+
     }
 }
