64e415dc07dd42f75e19a1b0e69a848e907f2dd1
Fabio Vincentelli <fabio.vincentelli@ynov.com>
2024-04-28 02:48:51 +0200

    corrected :     coins pick up bug after taking obstacle     power up pick up bug after taking obstacle

--- Assets/Scripts/Props/Activable/PlayerTrigger.cs
+++ Assets/Scripts/Props/Activable/PlayerTrigger.cs
@@ -5,6 +5,6 @@ public class PlayerTrigger : MonoBehaviour
     [SerializeField] Activable script;
 
     public void OnTriggerEnter(Collider other) {
-        if(other.gameObject.layer == Mathf.Log(GameManager.PlayerLayer,2))  script.Activate(other.gameObject);
+        if(other.gameObject.layer == Mathf.Log(GameManager.PlayerLayer,2) || other.gameObject.layer == LayerMask.NameToLayer("Ignoring"))  script.Activate(other.gameObject);
     }
 }
--- Assets/Scripts/Props/coinPickedUp.cs
+++ Assets/Scripts/Props/coinPickedUp.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 public class coinPickedUp : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.layer == Mathf.Log(GameManager.PlayerLayer.value, 2)) {
+        if (other.gameObject.layer == Mathf.Log(GameManager.PlayerLayer.value, 2) || other.gameObject.layer == LayerMask.NameToLayer("Ignoring")) {
 			GameManager.Instance.PlaySFX(SFXPlayer.SFX_TYPE.Controller);
 			other.GetComponent<PlayerScoring>().PlayerTookCoin();
             Destroy(gameObject);
